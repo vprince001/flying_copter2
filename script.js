5c0cfcf67f5bed30e800e251cd704a9a6d371185
Prince Verma <16836599+vprince001@users.noreply.github.com>
2019-01-09 10:42:33 +0530

    Flying Copter 2

---
+++ script.js
@@ -0,0 +1,114 @@
+const flyCopter = function() {
+  const copterStyle = document.getElementById("copter").style;
+  if (event.key == "ArrowUp") {
+    let location = +copterStyle.top.replace("px", "");
+    if (location > 0) {
+      location -= 10;
+      copterStyle.top = location + "px";
+    }
+  }
+
+  if (event.key == "ArrowDown") {
+    let location = +copterStyle.top.replace("px", "");
+    if (location < 590) {
+      location += 10;
+      copterStyle.top = location + "px";
+    }
+  }
+};
+
+const moveBar = function(divId) {
+  let leftPosition = +document
+    .getElementById(divId)
+    .style.left.replace("px", "");
+  return leftPosition - 1;
+};
+
+const getBarHeight = function(divId) {
+  let barHeight = 0;
+  if (divId < 7) {
+    barHeight = Math.floor(Math.random() * (400 - 150) + 150);
+  }
+  if (divId > 6) {
+    barHeight = Math.floor(Math.random() * (250 - 100) + 100);
+  }
+  return barHeight;
+};
+
+const setBarHeight = function(divId, barHeight) {
+  const barStyle = document.getElementById(divId).style;
+  barStyle.height = barHeight + "px";
+};
+
+const fire = function() {
+  const bulletStyle = document.getElementById("bullet").style;
+  const copterStyle = document.getElementById("copter").style;
+  const barStyle = document.getElementById("1").style;
+
+  let bulletY = +copterStyle.top.replace("px", "") + 50;
+  bulletStyle.top = bulletY + "px";
+
+  bulletStyle.height = "100px";
+
+  let moveBullet = setInterval(function() {
+    let bulletX = +bulletStyle.left.replace("px", "") + 2;
+    bulletStyle.left = bulletX + "px";
+
+    if (bulletX > 1225) {
+      bulletStyle.height = "0px";
+      bulletStyle.left = "100px";
+      clearInterval(moveBullet);
+    }
+    let barX = barStyle.left.replace("px", "");
+    let barY = barStyle.top.replace("px", "");
+
+    if (bulletX >= barX && bulletY <= barY) {
+      clearInterval(moveBullet);
+    }
+  }, 1);
+};
+
+const startGame = function() {
+  const copterStyle = document.getElementById("copter").style;
+  const bulletStyle = document.getElementById("bullet").style;
+  const barStyle = document.getElementById("1").style;
+
+  let copterX = 0;
+  let barHeight = 0;
+  let barX = 0;
+  const divId = 1;
+
+  let game = setInterval(function() {
+    barX = moveBar(divId);
+
+    if (barX < 0) {
+      barHeight = Math.floor(Math.random() * (400 - 150) + 150);
+      setBarHeight(divId, barHeight);
+      barX = 1200;
+    }
+    barStyle.left = barX + "px";
+    copterX = copterStyle.top.replace("px", "");
+
+    let bulletX = +bulletStyle.left.replace("px", "");
+    let bulletY = +bulletStyle.top.replace("px", "");
+
+    if (bulletX >= barX && bulletY <= barHeight) {
+      barStyle.height = "50px";
+    }
+
+    if (barX <= 65) {
+      if (copterX <= barHeight) {
+        clearInterval(game);
+      }
+    }
+  }, 1);
+};
+
+const main = function() {
+  if (event.key == "s") {
+    startGame();
+  }
+  if (event.key == " ") {
+    fire();
+  }
+};
